bc113f558bef148d34ec157c7fd91d8e9b44e7b8
Shubham <shubhuu1804@gmail.com>
2024-07-25 19:07:20 +0530

    Added SQL queries for the following : Activity - 1: Managing Customer Profile for Mark Tailor

--- dbms/script.sql
+++ dbms/script.sql
@@ -71,3 +71,101 @@ CREATE TABLE Payment (
     BillingAddress VARCHAR(100),
     FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
+
+USE ecommerceDATABASE;
+
+-- CREATING A NEW CUSTOMER
+INSERT INTO Customer (FirstName, MiddleName, LastName) 
+VALUES ('Mark', '', 'Tailor');
+ 
+ 
+ -- ASSIGNNING ROLE
+INSERT INTO CustomerRole (CustomerID, RoleID) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        (SELECT RoleID FROM Role WHERE RoleName='Customer'));
+        
+-- UPDATING THE NAME
+
+UPDATE Customer 
+SET MiddleName='K' 
+WHERE FirstName='Mark' AND LastName='Tailor';
+
+-- ALTERING THE CONTACT TABLE FOR AUTO INCREMENT
+
+ALTER TABLE Contact 
+MODIFY ContactID INT AUTO_INCREMENT;
+
+
+-- ADDING CONTACT INFORMATION
+
+INSERT INTO Contact (CustomerID, Email, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        'mark.tailor@example.com', 'Primary Email');
+
+-- ADDING A BILLING PHONE NUMBER 
+
+INSERT INTO Contact (CustomerID, Phone, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        '123-456-7890', 'Billing Phone');
+        
+-- ADDING A SHIPPING PHONE NUMBER
+
+INSERT INTO Contact (CustomerID, Phone, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        '098-765-4321', 'Shipping Number');
+        
+-- ALTERING THE ADRESS TABLE FOR AUTO INCREMENT
+
+ALTER TABLE Address 
+MODIFY AddressID INT AUTO_INCREMENT;
+
+        
+-- ADDING A SHIPPING ADDRESS
+
+INSERT INTO Address (CustomerID, StreetAddress, City, StateProvince, PostalCode, Country, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        '123 Elm St', 'Springfield', 'IL', '62704', 'USA', 'Shipping');
+
+-- ADDING A BILLING ADDRESS
+
+INSERT INTO Address (CustomerID, StreetAddress, City, StateProvince, PostalCode, Country, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        '123 Elm St', 'Springfield', 'IL', '62704', 'USA', 'Billing');
+        
+-- CHANGING THE PURPOSE TO General correspondence
+
+UPDATE Address 
+SET Purpose='General correspondence' 
+WHERE CustomerID=(SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor') 
+  AND Purpose='Billing';
+
+-- Delete the current email address and add a new one
+
+DELETE FROM Contact 
+WHERE CustomerID=(SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor') 
+  AND Purpose='Primary Email';
+  
+INSERT INTO Contact (CustomerID, Email, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        'new.email@rediffmail.com', 'Main Email');
+        
+-- Delete the current billing address and add a new one:
+
+DELETE FROM Address 
+WHERE CustomerID=(SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor') 
+  AND Purpose='General correspondence';
+  
+INSERT INTO Address (CustomerID, StreetAddress, City, StateProvince, PostalCode, Country, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        '456', 'arizona', 'IL', '62705', 'USA', 'General correspondence');
+        
+-- Delete the current shipping address and add a new one:
+
+DELETE FROM Address 
+WHERE CustomerID=(SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor') 
+  AND Purpose='Shipping';
+  
+INSERT INTO Address (CustomerID, StreetAddress, City, StateProvince, PostalCode, Country, Purpose) 
+VALUES ((SELECT CustomerID FROM Customer WHERE FirstName='Mark' AND LastName='Tailor'), 
+        '456 boulevard st.', '', 'IL', '62706', 'USA', 'Shipping');
+        
